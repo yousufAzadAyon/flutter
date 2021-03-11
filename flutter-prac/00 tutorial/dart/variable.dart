a05f382549475c584dd11da4d095a4dedccf1b75
yousuf azad ayon <52939358+yousufAzadAyon@users.noreply.github.com>
2021-03-12 00:06:07 +0600

    dart variables

---
+++ flutter-prac/00 tutorial/dart/variable.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  String name = 'Ayon';
+  var age = 24;
+  final height = 1.8;  
+
+  print('hello, im $name');
+  print('my name has ${name.length} letters');
+  print('my age is $age and my height is $height');
+
+}
