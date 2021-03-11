884f1f5c9b4e18b197fb6d1177fa7b62bcfb1ba9
yousuf azad ayon <52939358+yousufAzadAyon@users.noreply.github.com>
2021-03-12 00:08:23 +0600

    dart class

---
+++ flutter-prac/00 tutorial/dart/class.dart
@@ -0,0 +1,23 @@
+void main(List<String> args) {
+  var person = Person(name: 'ayon', age: 24, height: 1.8);
+  print(person.describe());
+
+}
+
+class Person{
+  Person(
+    {
+      this.name,
+      this.age,
+      this.height
+    }
+  );
+  
+  var name;
+  final age;
+  final height;
+
+  String describe()=>'hello my name is $name, im $age, and height is $height';
+
+}
+
